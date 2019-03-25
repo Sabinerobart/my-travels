372539b45d5d9eb4fc08492916645563747f8653
Sabine Robart <sabine.robart@protonmail.com>
2019-03-25 13:55:01 +0100

    Add files so that it works.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import img1 from './49c069bb-8a54-4ded-81f0-e463ef6c66da.hw1.jpeg';
+import img2 from './1280px-1_bali_rice_terrace_2011.jpeg';
 import './App.css';
+
+import Travels from "./Travels";
 
 class App extends Component {
   render() {
@@ -8,18 +12,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">My Travels</h1>
         </header>
+        <Travels
+          destination="Phoenix, Arizona"
+          country="U.S.A"
+          photo={img1}
+          distance="8724 kilometers"
+        />
+        <Travels
+          destination="Bali"
+          country="Indonesia"
+          photo={img2}
+          distance="12288 kilometers"
+        />
       </div>
     );
   }
